710aa62649b73532b870d9ed0e8d14f254ba9414
Anas <anasabdulmajeed08@gmail.com>
2019-09-06 00:43:02 +0300

    Completed the design of TO-DO app.

--- scripts/toDo.js
+++ scripts/toDo.js
@@ -1,1 +1,8 @@
-toDo.js
+let ourForm = document.getElementById("our-form");
+let ourField = document.getElementById('ourField');
+let ourField = document.getElementById('ourList');
+
+ourForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	console.log(ourField.value)
+})
